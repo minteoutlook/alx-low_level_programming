db14e781d2dfc1829ac4d54affc966081537452d
minteoutlook <mintesnotshiferaw29outlook.com>
2022-12-28 03:18:13 -0800

    5 and 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- *  * main - print out sizes of data type in c programming
- *   *
- *    * Return: 0
- *    */
+ * main - print out sizes of data type in c programming
+ * 
+ * Return: 0
+ */
 
 int main(void)
 {
-		char a;
-			int b;
-				long int c;
-					long long int d;
-						float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
 							printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 								printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
